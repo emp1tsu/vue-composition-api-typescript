bcbe181805aaaff60e3c7af26e40f5a8d080fc57
tanakakjs <oneok0813@gmail.com>
2020-07-04 21:59:50 +0900

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
     </div>
     <router-view />
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({});
+</script>
 
 <style>
 #app {
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,27 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>{{ message }}</h1>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/composition-api";
+
+type Props = {
+  message: string;
+};
+
+export default defineComponent({
+  props: {
+    message: {
+      type: String,
+      default: "default value"
+    }
+  },
+  setup(props: Props) {
+    return {
+      message: props.message
+    };
+  }
+});
+</script>
